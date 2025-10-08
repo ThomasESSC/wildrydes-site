9d6936e95d2b0c6550379edba4366d18b6f6bc20
ThomasESSC <thomas.enjalbert@soprasteria.com>
2025-10-08 09:23:21 +0200

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_fMkul0Fbi', 
-        userPoolClientId: '539dfgrgg7onkeevjvisjb3v47', 
-        region: 'ap-south-1' 
+        userPoolId: 'eu-west-1_UTpkdlaMm', 
+        userPoolClientId: '6hf6k3q7958ljkfo9r3u1da5mr', 
+        region: 'eu-west-1' 
     },
     api: {
         invokeUrl: 'https://b8hr1whxef.execute-api.ap-south-1.amazonaws.com/prod',
